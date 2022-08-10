231c0af6a1c110ee2358e502fa7dcc5d42a35db2
MakrFrost <frostmark@ukr.net>
2022-08-10 12:59:58 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  let parentIngredients = document.querySelector("#ingredients");
+  let item = document.createElement("li");
+  item.textContent = ingredients[i];
+  item.classList.add("item");
+  parentIngredients.append(item);
+}
