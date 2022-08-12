62399486319b45a03a45d30af657b320d6313777
MakrFrost <frostmark@ukr.net>
2022-08-12 14:11:10 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,6 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
 const button = document.querySelector(".change-color");
-const block = document.querySelector(".widget");
+const body = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+
+button.addEventListener("click", getRandomHexColor);
+
+function getRandomHexColor() {
+  let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  spanColor.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+}
