661c09d038730d531699c01bf433a5d9c929fe61
MakrFrost <frostmark@ukr.net>
2022-08-14 14:50:28 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,10 +1,38 @@
 const form = document.querySelector(".login-form");
-const email = document.querySelector("");
-const password = document.querySelector("");
-const buttonSubmit = document.querySelector("");
+const inputArray = document.querySelectorAll("input");
+const buttonSubmit = document.querySelector("button");
 
-email.addEventListener("blur");
-password.addEventListener("blur");
-buttonSubmit.addEventListener("click");
+// email.addEventListener("blur");
+// password.addEventListener("blur");
+// buttonSubmit.addEventListener("click", checkLogin);
 
-event.preventDefault();
+const formElements = event.currentTarget.elements;
+console.log(formElements);
+const email = formElements.email.value;
+console.log(email);
+const password = formElements.password.value;
+console.log(password);
+
+// function checkLogin() {
+//   event.preventDefault();
+// }
+const newInputArray = [...inputArray];
+// console.log(newInputArray);
+
+// const checkedInput = newInputArray.every((inputEl, newInputArray) =>
+//   console.log(inputEl)
+// );
+
+// console.log(checkedInput);
+
+form.addEventListener("submit", callbackForm);
+function callbackForm(event) {
+  event.preventDefault();
+}
+
+buttonSubmit.addEventListener("click", callbackCheck);
+function callbackCheck() {
+  if (checkedInput === true) {
+    console.log("gdsfgzdrsg");
+  }
+}
