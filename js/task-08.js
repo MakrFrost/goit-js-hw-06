956f6029b636f9c9f72cd99b36823541d0083ad7
MakrFrost <frostmark@ukr.net>
2022-08-14 23:02:20 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,21 +2,33 @@ const form = document.querySelector(".login-form");
 const inputArray = document.querySelectorAll("input");
 const buttonSubmit = document.querySelector("button");
 
+form.addEventListener("submit", callbackForm);
+
+function callbackForm(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((name, value) => {
+    console.log(name);
+    console.log(value);
+  });
+}
+
 // email.addEventListener("blur");
 // password.addEventListener("blur");
 // buttonSubmit.addEventListener("click", checkLogin);
 
-const formElements = event.currentTarget.elements;
-console.log(formElements);
-const email = formElements.email.value;
-console.log(email);
-const password = formElements.password.value;
-console.log(password);
+// const formElements = event.currentTarget.elements;
+// console.log(formElements);
+// const email = formElements.email.value;
+// console.log(email);
+// const password = formElements.password.value;
+// console.log(password);
 
 // function checkLogin() {
 //   event.preventDefault();
 // }
-const newInputArray = [...inputArray];
+// const newInputArray = [...inputArray];
 // console.log(newInputArray);
 
 // const checkedInput = newInputArray.every((inputEl, newInputArray) =>
@@ -25,14 +37,9 @@ const newInputArray = [...inputArray];
 
 // console.log(checkedInput);
 
-form.addEventListener("submit", callbackForm);
-function callbackForm(event) {
-  event.preventDefault();
-}
-
-buttonSubmit.addEventListener("click", callbackCheck);
-function callbackCheck() {
-  if (checkedInput === true) {
-    console.log("gdsfgzdrsg");
-  }
-}
+// buttonSubmit.addEventListener("click", callbackCheck);
+// function callbackCheck() {
+//   if (checkedInput === true) {
+//     console.log("gdsfgzdrsg");
+//   }
+// }
