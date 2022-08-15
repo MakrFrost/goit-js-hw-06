54f4a707844d52eb19d1174c8ee63c06c28deba5
MakrFrost <frostmark@ukr.net>
2022-08-15 22:10:35 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -7,42 +7,11 @@ form.addEventListener("submit", callbackForm);
 function callbackForm(event) {
   event.preventDefault();
 
-  const formData = new FormData(event.currentTarget);
-  formData.forEach((name, value) => {
-    return console.log({ name: value, value: name });
-  });
+  const form = event.currentTarget.elements;
+  const email = form.email.value;
+  const password = form.password.value;
+
+  if (email === "" || password === "") {
+    alert("Заполните все поля!");
+  } else return console.log({ email: email, password: password });
 }
-
-// const formElements = event.currentTarget.elements;
-// const email = formElements.email.value;
-// const password = formElements.password.value;
-
-// email.addEventListener("blur");
-// password.addEventListener("blur");
-// buttonSubmit.addEventListener("click", checkLogin);
-
-// const formElements = event.currentTarget.elements;
-// console.log(formElements);
-// const email = formElements.email.value;
-// console.log(email);
-// const password = formElements.password.value;
-// console.log(password);
-
-// function checkLogin() {
-//   event.preventDefault();
-// }
-// const newInputArray = [...inputArray];
-// console.log(newInputArray);
-
-// const checkedInput = newInputArray.every((inputEl, newInputArray) =>
-//   console.log(inputEl)
-// );
-
-// console.log(checkedInput);
-
-// buttonSubmit.addEventListener("click", callbackCheck);
-// function callbackCheck() {
-//   if (checkedInput === true) {
-//     console.log("gdsfgzdrsg");
-//   }
-// }
